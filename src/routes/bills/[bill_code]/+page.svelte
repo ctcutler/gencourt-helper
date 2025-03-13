b602eb5ddec5bf8d23ecb8b6795ca23607360664
Chris Cutler <ctcutler@gmail.com>
2025-03-12 20:33:23 -0400

    format date, clean up code

--- src/routes/bills/[bill_code]/+page.svelte
+++ src/routes/bills/[bill_code]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	let { data } = $props();
 
+	function formatDate(d: Date): string {
+		return d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
+	}
+
 	const bill = data.bills.find((x) => x.bill_code == data.selected_bill_code);
 	let upcoming_hearing_date: Date | null = $state(null);
 	const now = new Date();
@@ -14,15 +18,20 @@
 </script>
 
 {#if bill}
- 
-{#if upcoming_hearing_date}
-  <div><p>Upcoming public hearing on: {upcoming_hearing_date}</p></div>
-{/if}
-<iframe
-	class="h-screen w-full"
-	title="bill info"
-	src="https://gc.nh.gov/bill_status/billinfo.aspx?id={bill?.id}&inflect=2"
-></iframe>
+	{#if upcoming_hearing_date}
+		<div>
+			<p>
+				Upcoming public hearing on: {formatDate(upcoming_hearing_date)}. Click here to leave
+				testimony/indicate your support or opposition.
+			</p>
+		</div>
+	{/if}
+
+	<iframe
+		class="h-screen w-full"
+		title="bill info"
+		src="https://gc.nh.gov/bill_status/billinfo.aspx?id={bill?.id}&inflect=2"
+	></iframe>
 {:else}
-<p>Unable to find {data.selected_bill_code}</p>
+	<p>Unable to find {data.selected_bill_code}</p>
 {/if}
