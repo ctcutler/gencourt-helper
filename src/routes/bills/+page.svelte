16ee5af89f5c90192dc6d3755d8c4a8bfc6b6eb0
Chris Cutler <ctcutler@gmail.com>
2025-03-17 16:02:51 -0400

    Style /bills

--- src/routes/bills/+page.svelte
+++ src/routes/bills/+page.svelte
@@ -2,10 +2,12 @@
 	let { data } = $props();
 </script>
 
-<h1>List of bills</h1>
+<div class="p-5 max-w-4xl">
+	<h1 class="font-bold text-2xl p-2">Bills & Resolutions from the Current Legislative Session</h1>
 
-<ul>
-	{#each data.bills as bill}
-		<li><a href="/bills/{bill.bill_code}">{bill.bill_code} ({bill.title})</a></li>
-	{/each}
-</ul>
+	<ul>
+		{#each data.bills as bill}
+			<li class="p-2"><a href="/bills/{bill.bill_code}" class="font-bold underline">{bill.bill_code}</a>: {bill.title}</li>
+		{/each}
+	</ul>	
+</div>
