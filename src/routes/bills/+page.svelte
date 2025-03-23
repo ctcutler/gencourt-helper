ecf8195916951d24b1cff3ac334459edc5a6ba40
Chris Cutler <ctcutler@gmail.com>
2025-03-23 13:50:22 -0400

    Center content in the hearing list.

--- src/routes/bills/+page.svelte
+++ src/routes/bills/+page.svelte
@@ -2,7 +2,7 @@
 	let { data } = $props();
 </script>
 
-<div class="bg-yellow-200 p-2">
+<div class="flex justify-center bg-yellow-200 p-2">
 	<p class="max-w-4xl">
 		Welcome to the General Court Helper, a tool to make it easier to find and give feedback on bills
 		being considered by New Hampshire's state legislature (a.k.a. the General Court). Click on the
@@ -10,29 +10,31 @@
 	</p>
 </div>
 
-<div class="max-w-4xl p-5">
-	<h1 class="text-3xl font-bold">Scheduled Hearings</h1>
-	<ul>
-		{#each data.hearings as [ committee, dates ]}
-			<li>
-				<h2 class="pt-6 text-xl font-bold">{committee} Committee</h2>
-				<ul>
-					{#each dates as [date, bills]}
-						<li>
-							<h3 class="text-l pt-2 pl-4 italic">{date}</h3>
-							<ul>
-								{#each bills as bill}
-									<li class="pl-8">
-										<a href="/bills/{bill.bill_code}" class="font-bold text-blue-600"
-											>{bill.bill_code}</a
-										>: {bill.title}
-									</li>
-								{/each}
-							</ul>
-						</li>
-					{/each}
-				</ul>
-			</li>
-		{/each}
-	</ul>
+<div class="flex justify-center">
+	<div class="max-w-4xl p-5">
+		<h1 class="text-3xl font-bold">Scheduled Hearings</h1>
+		<ul>
+			{#each data.hearings as [committee, dates]}
+				<li>
+					<h2 class="pt-6 text-xl font-bold">{committee} Committee</h2>
+					<ul>
+						{#each dates as [date, bills]}
+							<li>
+								<h3 class="text-l pt-2 pl-4 italic">{date}</h3>
+								<ul>
+									{#each bills as bill}
+										<li class="pl-8">
+											<a href="/bills/{bill.bill_code}" class="font-bold text-blue-600"
+												>{bill.bill_code}</a
+											>: {bill.title}
+										</li>
+									{/each}
+								</ul>
+							</li>
+						{/each}
+					</ul>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
