3debf52cb4356e35b0d6c4d7d1f00eade3b0a7b3
Chris Cutler <ctcutler@gmail.com>
2025-04-18 14:02:00 -0400

    Try again with regex.escape

--- src/routes/bills/+layout.server.ts
+++ src/routes/bills/+layout.server.ts
@@ -1,6 +1,9 @@
 import type { LayoutServerLoad } from './$types';
 import { DateTime } from 'luxon';
 
+// also: this is temporary... when Regex.escape() support is widespread we'll switch to that
+import regexescape from 'regexp.escape';
+
 // yes, this state is shared by all users; that's ok
 let bills: Array<Bill>;
 let hearings: Map<string, Map<string, Array<Bill>>>;
@@ -65,7 +68,7 @@ export function _parse_date_from_description(description: string): Date {
 export function _parse_committee_from_description(committees: Array<string>, description: string): string {
   if (description) {
     for (const committee of committees) {
-      if (description.search(new RegExp(committee)) != -1) {
+      if (description.search(new RegExp(regexescape(committee))) != -1) {
         return committee;
       }
     }
